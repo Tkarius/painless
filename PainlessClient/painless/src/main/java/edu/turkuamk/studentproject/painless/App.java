b64af121c39e2693fa40500da660d513280fb0d4
painless <painless@painless-dev.painless.edu>
2017-10-09 20:35:07 +0300

    Initial commit for Painless.

---
+++ PainlessClient/painless/src/main/java/edu/turkuamk/studentproject/painless/App.java
@@ -0,0 +1,13 @@
+package edu.turkuamk.studentproject.painless;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
