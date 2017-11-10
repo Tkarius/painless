4e821ca2b0be5ce6c522029d3cf4dbe613e36a6b
mpohjola <mira.pohjola89@gmail.com>
2017-11-10 20:29:05 +0200

    Initial structure for JavaFX GUI with FXML documents and controllers for Login screen and main window. Main window mostly unfinished, but login roughly works. Login doesn't yet check authentication from server.

---
+++ PainlessClient/painless/src/main/java/edu/turkuamk/studentproject/painless/LoginController.java
@@ -0,0 +1,45 @@
+package edu.turkuamk.studentproject.painless;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+/**
+ * Controller for login.fxml. Captures login information, asks for auth from server.
+ * Passes the auth information to WindowManager.
+ * 
+ * @author Mira Pohjola
+ * 
+ */
+public class LoginController {
+	@FXML private TextField user;
+  @FXML private TextField password;
+  @FXML private Button loginButton;
+  @FXML private Button registerButton;
+  @FXML private Text testtarget;
+
+  
+  public void initManager(final WindowManager windowManager) {
+    loginButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent event) {
+      	System.out.println("Login Button pressed.");
+      	Credentials.setPass(password.getText());
+      	Credentials.setUser(user.getText());
+      	System.out.println(Credentials.getUser() + Credentials.getPass());
+      	Boolean auth = authorized();
+        	windowManager.authenticated(auth);
+      }
+
+			private boolean authorized() {
+				//Send user credentials over MQTT for verification. Return true if user with that password/username exists
+				//And false if doesn't
+				return true;
+			}
+    });
+  }
+
+  
+
+  
+}
---
+++ PainlessClient/painless/src/main/java/edu/turkuamk/studentproject/painless/MainWindowController.java
@@ -0,0 +1,35 @@
+package edu.turkuamk.studentproject.painless;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+/**
+ * MainWindowController class is a controller for mainWindow.fxml. Handles MQTT connectivity and main window
+ * functionality.
+ * 
+ * @author Mira Pohjola
+ * 
+ */
+
+public class MainWindowController {
+	@FXML private Text user;
+	private static final MqttConnection mqtt = new MqttConnection();
+
+	/**
+	 * Initializes the user dashboard with user related information, such as username and
+	 * the list of channels user has subscribed to.
+	 * 
+	 * @author Mira Pohjola
+	 */
+	public void initDashboard(WindowManager windowManager) {
+		//TODO: Basically almost everything. 
+		//Right now we show username, open MQTT connection, send one message and shut the connection. Wohoo!
+		user.setText(Credentials.getUser());
+		mqtt.mqttOpen();
+    mqtt.sendMessage("testi/t1", "Hello wurld!");
+    //menu();
+    mqtt.mqttClose();
+		
+	}
+
+}
