b7d4507f8d5650183d4ce4fdb09ef70daea5b41b
mpohjola <mira.pohjola89@gmail.com>
2017-11-11 16:47:00 +0200

    Poked Main Window further together. Added a dummy class to represent Channel(s) and some dummy logic to show chat messages and channel listings. For some reason the main window insists on rendering as very small. Will look into it later to set it to render itself in better size.

--- PainlessClient/painless/src/main/java/edu/turkuamk/studentproject/painless/MainWindowController.java
+++ PainlessClient/painless/src/main/java/edu/turkuamk/studentproject/painless/MainWindowController.java
@@ -1,7 +1,14 @@
 package edu.turkuamk.studentproject.painless;
 
+
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 /**
@@ -17,6 +24,10 @@ public class MainWindowController {
 	@FXML private Text loggedInAsText;
 	@FXML private Button logoutButton;
 	@FXML private Button settingsButton;
+	@FXML private GridPane channelList;
+	@FXML private ScrollPane chatScreen;
+	@FXML private GridPane chatBox;
+	private Channel activeChannel;
 	
 	private static final MqttConnection mqtt = new MqttConnection();
 
@@ -29,13 +40,42 @@ public class MainWindowController {
 	public void initDashboard(WindowManager windowManager) {
 		//TODO: Basically almost everything. 
 		//Right now we show username, open MQTT connection, send one message and shut the connection. Wohoo!
-		//user.setText(Credentials.getUser());
+		
+		//Following things mostly exist for testing the layout.
+		ArrayList<Channel> testChannelList = new ArrayList<Channel>();
+		testChannelList.add(new Channel("painless/parsat/on/parhaita", "Owner"));
+		populateChannelList(testChannelList);	
+		ArrayList<String> testMessageList = new ArrayList<String>();
+		testMessageList.add("Olen Viesti");
+		testMessageList.add("Olen myös viesti");
+		showChat(testMessageList);
+		
 		loggedInAsText.setText("Signed in as: " + Credentials.getUser());
 		mqtt.mqttOpen();
     mqtt.sendMessage("testi/t1", "Hello wurld!");
     //menu();
-    mqtt.mqttClose();
-		
+    mqtt.mqttClose();		
+	}
+	
+	//Current implementation here wants a ArrayList full of channels, but if we decide to handle channels some different way
+	//let's change this!
+	private void populateChannelList(ArrayList<Channel> channels) {		
+		for (int i = 0; i < channels.size(); i++) {
+			channelList.add(new Label(channels.get(i).getcName()), 0, i);
+			channelList.add(new Label(channels.get(i).getRole()), 1, i);
+			//TODO: Add event listener etc. to button
+			Button manageButton = new Button("Manage");
+			channelList.add(manageButton, 2, i);
+		}		
+	}
+	
+	//When we will have a class for messages, I presume we will want this method to take an ArrayList of those instead.
+	//This is purely for layout testing.
+	private void showChat(ArrayList<String> messages) {
+		for (int i = 0; i < messages.size(); i++) {
+			chatBox.add(new Label("username"), 0, i);
+			chatBox.add(new Label(messages.get(i)), 1, i);
+		}
 	}
 
 }
