7d4b5ada1cab0fb9a42b27828761f6079aef70f7
Tommi Tuomola <tommi.t.tuomola@gmail.com>
2017-11-19 15:33:56 +0200

    Renamed and modified Channel to PainlessChannel. We now store msgs under the channel class.

--- PainlessClient/painless/src/main/java/edu/turkuamk/studentproject/painless/PainlessChannel.java
+++ PainlessClient/painless/src/main/java/edu/turkuamk/studentproject/painless/PainlessChannel.java
@@ -3,23 +3,23 @@ package edu.turkuamk.studentproject.painless;
 //TODO: This class is a suggestion on how we want to handle information about each of user's subscribed channels.
 //We could save a list of these guys in some kind of 'user' class to be used in populating the list (reading the channels
 //from file), saving the list of channels to file and in populating the main window with the channels?
-public class Channel {
+public class PainlessChannel {
   private String cName = "";
   private String role = "Client";
   // We'll implement the channel messages as a queue of 50 Strings in a static array.
   private final int CHANNEL_MAX_MESSAGES = 51;
-  private String[] messages = new String[CHANNEL_MAX_MESSAGES]; 
+  private PainlessMessage[] messages = new PainlessMessage[CHANNEL_MAX_MESSAGES]; 
   private int msgTail = 0;  // index to add the next message into
   private int msgHead = 0;  // index of the oldest message
 	
   // constructor
-  public Channel(String channelName, String _role) {
+  public PainlessChannel(String channelName, String _role) {
     setcName(channelName);
     setRole(_role);
     // TODO: should read stored messages from a file in constructor
   }
 	
-  public void addMsg(String msg) {
+  public void addMsg(PainlessMessage msg) {
     int addToIndex = msgTail;
     if (addToIndex == CHANNEL_MAX_MESSAGES - 1) {
       addToIndex = 0;
